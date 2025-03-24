43ba0875c799a0ab76777dc29b790d31788d7417
limkanghyun <limkh06910691@gmail.com>
2025-03-24 16:51:43 +0900

    feat: Application 클래스 기본 구조 구현
    - 사용자 입력 처리 및 URI 생성 기능 추가 ("/post/" + command, 추후 수정 예정)
    - PostController로 명령어 전달 로직 구현
    - ".exit" 명령어로 프로그램 종료 처리

--- src/main/java/sys/Application.java
+++ src/main/java/sys/Application.java
@@ -1,5 +1,7 @@
 package sys;
 
+import controller.PostController;
+
 import java.util.Scanner;
 
 public class Application {
@@ -13,7 +15,11 @@ public class Application {
     }
 
     public void run() {
+
+        PostController postController = new PostController();
+
         while(programStatus) {
+
             String line = "https://" + domain;
 
             System.out.print(line);
@@ -24,12 +30,19 @@ public class Application {
                 break;
             }
 
-            Request request = new Request(command);
+            Request request = new Request("/post" + command);
+
             if (!request.isValid()) {
                 System.out.println("잘못된 형식의 입력입니다.");
+                System.out.println("입력된 URI: " + command); // 디버깅
+                System.out.println("target: " + request.getTarget()); // 여기가 "add"여야 함
                 continue;
             }
             System.out.println("명령 통과!");
+
+            // Request 객체 생성 및 PostController로 전달
+            postController.requestHandler(request);
+
 
         }
     }
